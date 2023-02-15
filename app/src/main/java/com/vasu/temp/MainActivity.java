5c1eccdd669af2eb6065110b96d3ec2519adab87
Vaibhav Kolte <vaibhav.kolte@exteral.forvia.com>
2023-02-15 09:24:30 +0530

    add RecyclerViewActivity

--- app/src/main/java/com/vasu/temp/MainActivity.java
+++ app/src/main/java/com/vasu/temp/MainActivity.java
@@ -2,13 +2,25 @@ package com.vasu.temp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getName();
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "onCreate: " );
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            startActivity(new Intent(this,RecyclerViewActivity.class));
+        });
     }
 }
